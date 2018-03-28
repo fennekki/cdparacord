25ef43d1772ed8817073aa77d76af9e60d716b6b
fennekki <fennekki@iki.fi>
2018-03-28 06:12:07 +0300

    Add tests for properties of Dependency
    They are as of now dummy tests but so are the properties: If they ever
    stop being functions they can be eliminated here, and if they become
    less-than-dummy they can be made less dummy here.

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -19,6 +19,7 @@ def mock_external_binary():
         os.chmod(f.name, stat.S_IXUSR)
         yield f.name
 
+
 @pytest.fixture
 def mock_config_external():
     """Mock the Config class such that it always returns given name."""
@@ -32,6 +33,7 @@ def mock_config_external():
                 return mockbin
         return MockConfig()
     return get_config
+
 
 def test_find_valid_absolute_dependencies(mock_external_binary, mock_config_external):
     """Finds fake dependencies that exist by absolute path."""
@@ -47,9 +49,34 @@ def test_find_valid_dependencies_in_path(mock_external_binary, mock_config_exter
     monkeypatch.setenv("PATH", dirname)
     Dependency(mock_config_external(basename))
 
+
 def test_fail_to_find_dependencies(mock_config_external):
     with NamedTemporaryFile(prefix='cdparacord-unittest-') as f:
         # This file should not be executable by default so the finding
         # should fail
         with pytest.raises(DependencyError):
             Dependency(mock_config_external(f.name))
+
+
+def test_get_encoder(mock_config_external, mock_external_binary):
+    """Get the 'encoder' property."""
+
+    deps = Dependency(mock_config_external(mock_external_binary))
+    # It's an absolute path so the value should be the same
+    assert deps.encoder == mock_external_binary
+
+
+def test_get_editor(mock_config_external, mock_external_binary):
+    """Get the 'editor' property."""
+
+    deps = Dependency(mock_config_external(mock_external_binary))
+    # It's an absolute path so the value should be the same
+    assert deps.editor == mock_external_binary
+
+
+def test_get_cdparanoia(mock_config_external, mock_external_binary):
+    """Get the 'cdparanoia' property."""
+
+    deps = Dependency(mock_config_external(mock_external_binary))
+    # It's an absolute path so the value should be the same
+    assert deps.cdparanoia == mock_external_binary

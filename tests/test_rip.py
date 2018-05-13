8361c5adbecee20df174456dde6f6a3fe34d6ac7
fennekki <fennekki@iki.fi>
2018-05-13 19:43:30 +0300

    Add test coverage for _rip_track

--- tests/test_rip.py
+++ tests/test_rip.py
@@ -2,11 +2,40 @@ import pytest
 import asyncio
 from cdparacord import rip
 
+@pytest.fixture
+def get_fake_config():
+    class FakeConfig:
+        def __init__(self):
+            self.fail_one = False
+            self.fail_all = False
+
+        def get(self, key):
+            if key in ('post_rip', 'post_encode'):
+                if self.fail_one:
+                    return [{'false': ['${one_file}']}]
+                else:
+                    return [{'echo': ['${one_file}']}]
+            elif key == 'post_finished':
+                if self.fail_one:
+                    return [{'false': ['${one_file}']}]
+                elif self.fail_all:
+                    return [{'false': ['${all_files}']}]
+                else:
+                    return [{'echo': ['${one_file}', '${all_files}']},{'echo': ['$all_files']}]
+            elif key == 'encoder':
+                if self.fail_one:
+                    return {'false': ['${one_file}', '${out_file}']}
+                else:
+                    return {'echo': ['${one_file}', '${out_file}']}
+            else:
+                return ''
+    yield FakeConfig
+
 def test_construct_rip(monkeypatch):
     """Test constructing Rip object."""
     rip.Rip(None, None, None, 1, 1, True)
 
-def test_rip_pipeline(monkeypatch):
+def test_rip_pipeline(monkeypatch, get_fake_config):
     """Test constructing Rip and running first pipeline function."""
     class FakeTrack:
         def __init__(self):
@@ -30,26 +59,6 @@ def test_rip_pipeline(monkeypatch):
     class FakeDeps:
         ...
 
-    class FakeConfig:
-        def __init__(self):
-            self.fail_post_finished_one = False
-            self.fail_post_finished_all = False
-
-        def get(self, key):
-            if key in ('post_rip', 'post_encode'):
-                return [{'echo': ['${one_file}']}]
-            elif key == 'post_finished':
-                if self.fail_post_finished_one:
-                    return [{'false': ['${one_file}']}]
-                elif self.fail_post_finished_all:
-                    return [{'false': ['${all_files}']}]
-                else:
-                    return [{'echo': ['${one_file}', '${all_files}']},{'echo': ['$all_files']}]
-            elif key == 'encoder':
-                return {'echo': ['${one_file}', '${out_file}']}
-            else:
-                return ''
-
     async def fake_rip(self, track):
         await fake_encode(self, track, track.filename)
 
@@ -64,7 +73,7 @@ def test_rip_pipeline(monkeypatch):
     monkeypatch.setattr('os.makedirs', lambda x, exist_ok: None)
     monkeypatch.setattr('shutil.copy2', lambda x, y: True)
 
-    fake_config = FakeConfig()
+    fake_config = get_fake_config()
     r = rip.Rip(FakeAlbumdata(), FakeDeps(), fake_config, 1, 1, True)
 
     # Use isfile to cover both paths
@@ -78,22 +87,82 @@ def test_rip_pipeline(monkeypatch):
     r.rip_pipeline()
 
     # Make the post_rip tasks fail
-    fake_config.fail_post_finished_one = True
-    fake_config.fail_post_finished_all = True
+    fake_config.fail_one = True
+    fake_config.fail_all = True
     asyncio.set_event_loop(asyncio.new_event_loop())
     with pytest.raises(rip.RipError):
         r.rip_pipeline()
     asyncio.get_event_loop().close()
-    fake_config.fail_post_finished_one = False
+    fake_config.fail_one = False
     # Test the other fail
     asyncio.set_event_loop(asyncio.new_event_loop())
     with pytest.raises(rip.RipError):
         r.rip_pipeline()
     asyncio.get_event_loop().close()
-    fake_config.fail_post_finished_all = False
+    fake_config.fail_all = False
 
     # Track number past the range (and as such: empty ripped tracks)
     fake_track.tracknumber = 2
     asyncio.set_event_loop(asyncio.new_event_loop())
     r.rip_pipeline()
     fake_track.tracknumber = 1
+
+
+def test_rip_track(monkeypatch, get_fake_config):
+    class FakeTrack:
+        def __init__(self):
+            self.tracknumber = 1
+
+        @property
+        def filename(self):
+            return '/tmp/oispa-kaljaa/final/test.mp3'
+
+    fake_track = FakeTrack()
+
+    class FakeAlbumdata:
+        @property
+        def tracks(self):
+            return [fake_track]
+
+        @property
+        def ripdir(self):
+            return '/tmp/oispa-kaljaa'
+
+    class FakeDeps:
+        def __init__(self):
+            self.cdparanoia = 'echo'
+
+    fake_config = get_fake_config()
+    fake_deps = FakeDeps()
+    r = rip.Rip(FakeAlbumdata(), fake_deps, fake_config, 1, 1, True)
+
+    async def fake_encode(self, track, filename):
+        ...
+        
+    monkeypatch.setattr('cdparacord.rip.Rip._encode_track', fake_encode)
+    monkeypatch.setattr('os.rename', lambda x, y: True)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(r._rip_track(fake_track))
+    loop.close()
+
+    # Fail ripping track
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    fake_deps.cdparanoia = 'false'
+    with pytest.raises(rip.RipError):
+        loop.run_until_complete(r._rip_track(fake_track))
+    fake_deps.cdparanoia = 'echo'
+    loop.close()
+
+
+    # Fail running post-rip task
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    fake_config.fail_one = True
+    with pytest.raises(rip.RipError):
+        loop.run_until_complete(r._rip_track(fake_track))
+    fake_config.fail_one = False
+    loop.close()
+

068356a5dfe0cddfe98d02ab49b5081fa61c869a
fennekki <fennekki@iki.fi>
2018-05-13 19:02:39 +0300

    Finish test coverage of rip_pipeline

--- tests/test_rip.py
+++ tests/test_rip.py
@@ -9,14 +9,19 @@ def test_construct_rip(monkeypatch):
 def test_rip_pipeline(monkeypatch):
     """Test constructing Rip and running first pipeline function."""
     class FakeTrack:
+        def __init__(self):
+            self.tracknumber = 1
+
         @property
-        def tracknumber(self):
-            return 1
+        def filename(self):
+            return '/tmp/oispa-kaljaa/final/test.mp3'
+
+    fake_track = FakeTrack()
 
     class FakeAlbumdata:
         @property
         def tracks(self):
-            return [FakeTrack()]
+            return [fake_track]
 
         @property
         def ripdir(self):
@@ -26,11 +31,20 @@ def test_rip_pipeline(monkeypatch):
         ...
 
     class FakeConfig:
+        def __init__(self):
+            self.fail_post_finished_one = False
+            self.fail_post_finished_all = False
+
         def get(self, key):
             if key in ('post_rip', 'post_encode'):
                 return [{'echo': ['${one_file}']}]
             elif key == 'post_finished':
-                return [{'echo': ['${one_file}', '${all_files}']}]
+                if self.fail_post_finished_one:
+                    return [{'false': ['${one_file}']}]
+                elif self.fail_post_finished_all:
+                    return [{'false': ['${all_files}']}]
+                else:
+                    return [{'echo': ['${one_file}', '${all_files}']},{'echo': ['$all_files']}]
             elif key == 'encoder':
                 return {'echo': ['${one_file}', '${out_file}']}
             else:
@@ -50,13 +64,36 @@ def test_rip_pipeline(monkeypatch):
     monkeypatch.setattr('os.makedirs', lambda x, exist_ok: None)
     monkeypatch.setattr('shutil.copy2', lambda x, y: True)
 
-    r = rip.Rip(FakeAlbumdata(), FakeDeps(), FakeConfig(), 1, 1, True)
+    fake_config = FakeConfig()
+    r = rip.Rip(FakeAlbumdata(), FakeDeps(), fake_config, 1, 1, True)
 
-    # Use isfile to cover both paths 
+    # Use isfile to cover both paths
     asyncio.set_event_loop(asyncio.new_event_loop())
     monkeypatch.setattr('os.path.isfile', lambda x: True)
     r.rip_pipeline()
 
+    # This one will schedule a rip, not an encode
     asyncio.set_event_loop(asyncio.new_event_loop())
-    monkeypatch.setattr('os.path.isfile', lambda x: True)
+    monkeypatch.setattr('os.path.isfile', lambda x: False)
     r.rip_pipeline()
+
+    # Make the post_rip tasks fail
+    fake_config.fail_post_finished_one = True
+    fake_config.fail_post_finished_all = True
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    with pytest.raises(rip.RipError):
+        r.rip_pipeline()
+    asyncio.get_event_loop().close()
+    fake_config.fail_post_finished_one = False
+    # Test the other fail
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    with pytest.raises(rip.RipError):
+        r.rip_pipeline()
+    asyncio.get_event_loop().close()
+    fake_config.fail_post_finished_all = False
+
+    # Track number past the range (and as such: empty ripped tracks)
+    fake_track.tracknumber = 2
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    r.rip_pipeline()
+    fake_track.tracknumber = 1

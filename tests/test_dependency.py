cabdd0a969428f31094242c6f236feffdad7f47a
fennekki <fennekki@iki.fi>
2018-07-14 12:37:33 +0300

    Verify action configuration params are strings
    First of all, we implement tests that ensure these conditions for the
    default configuration:
    
    1. Actions means encoder, as well as all the post_rip, post_encode and
       post_finished actions.
    2. For each action, there must be exactly one dict key. Under this dict
       key must be a list containing only string values.
    
    Then, we implement Dependency._verify_action_params which tests these
    conditions for runtime config. Then, we implement tests for
    _verify_action_params that take this into action.
    
    In addition to this, the test fixtures in test_dependency have been
    refactored to be slightly easier to use and to be more suitable for the
    new tests. This required a minor change to most tests.

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -9,45 +9,57 @@ from cdparacord.dependency import Dependency, DependencyError
 
 
 @pytest.fixture
-def mock_external_binary():
-    """Mock an external dependency binary.
-    
-    Create a file, set it to be executable and return it as an
-    ostensible external binary.
+def mock_config_external():
+    """Mock the Config class such that it returns self.param.
+
+    In addition, querying for  encoder results in a dict that contains
+    an empty list in the key self.param.
     """
-    with NamedTemporaryFile(prefix='cdparacord-unittest-') as f:
-        os.chmod(f.name, stat.S_IXUSR)
-        yield f.name
+    class MockConfig:
+        def __init__(self, param):
+            self.param = param
+            self.encoder = {self.param: []}
+            self.post = [{self.param: []}]
+
+        def get(self, name):
+            # Maybe we should write a fake config file but there are
+            # Huge issues with mocking the config module...
+            if name == 'encoder':
+                return self.encoder
+            if name in ('post_rip', 'post_encode', 'post_finished'):
+                return self.post
+            return self.param
+    return MockConfig
 
 
 @pytest.fixture
-def mock_config_external():
-    """Mock the Config class such that it always returns given name."""
-    def get_config(mockbin):
-        class MockConfig:
-            def get(self, name):
-                # Maybe we should write a fake config file but there are
-                # Huge issues with mocking the config module...
-                if name == 'encoder':
-                    return {mockbin: []}
-                return mockbin
-        return MockConfig()
-    return get_config
+def mock_external_encoder(mock_config_external):
+    """Mock an external dependency binary.
+
+    Create a file, set it to be executable and return it as an
+    ostensible external binary via configuration.
+    """
+    with NamedTemporaryFile(prefix='cdparacord-unittest-') as f:
+        os.chmod(f.name, stat.S_IXUSR)
+
+        conf = mock_config_external(f.name)
+        yield conf
 
 
-def test_find_valid_absolute_dependencies(mock_external_binary, mock_config_external):
+def test_find_valid_absolute_dependencies(mock_external_encoder):
     """Finds fake dependencies that exist by absolute path."""
-    
-    Dependency(mock_config_external(mock_external_binary))
+    Dependency(mock_external_encoder)
 
 
-def test_find_valid_dependencies_in_path(mock_external_binary, mock_config_external, monkeypatch):
+def test_find_valid_dependencies_in_path(mock_external_encoder, monkeypatch):
     """Finds fake dependencies that exist in $PATH."""
 
-    dirname, basename = os.path.split(mock_external_binary)
+    dirname, basename = os.path.split(mock_external_encoder.param)
     # Set PATH to only contain the directory our things are in
     monkeypatch.setenv("PATH", dirname)
-    Dependency(mock_config_external(basename))
+    conf = mock_external_encoder
+    conf.param = basename
+    Dependency(conf)
 
 
 def test_fail_to_find_dependencies(mock_config_external):
@@ -55,28 +67,59 @@ def test_fail_to_find_dependencies(mock_config_external):
         # This file should not be executable by default so the finding
         # should fail
         with pytest.raises(DependencyError):
-            Dependency(mock_config_external(f.name))
+            conf = mock_config_external(f.name)
+            Dependency(conf)
 
 
-def test_get_encoder(mock_config_external, mock_external_binary):
+def test_get_encoder(mock_external_encoder):
     """Get the 'encoder' property."""
 
-    deps = Dependency(mock_config_external(mock_external_binary))
+    deps = Dependency(mock_external_encoder)
     # It's an absolute path so the value should be the same
-    assert deps.encoder == mock_external_binary
+    assert deps.encoder == mock_external_encoder.param
 
 
-def test_get_editor(mock_config_external, mock_external_binary):
+def test_get_editor(mock_external_encoder):
     """Get the 'editor' property."""
 
-    deps = Dependency(mock_config_external(mock_external_binary))
+    deps = Dependency(mock_external_encoder)
     # It's an absolute path so the value should be the same
-    assert deps.editor == mock_external_binary
+    assert deps.editor == mock_external_encoder.param
 
 
-def test_get_cdparanoia(mock_config_external, mock_external_binary):
+def test_get_cdparanoia(mock_external_encoder):
     """Get the 'cdparanoia' property."""
 
-    deps = Dependency(mock_config_external(mock_external_binary))
+    deps = Dependency(mock_external_encoder)
     # It's an absolute path so the value should be the same
-    assert deps.cdparanoia == mock_external_binary
+    assert deps.cdparanoia == mock_external_encoder.param
+
+def test_verify_action_params(mock_external_encoder):
+    """Ensure encoder parameter verification works."""
+
+    conf = mock_external_encoder
+    deps = Dependency(conf)
+
+    # Dict can only have one key
+    invalid_input = {'a': [], 'b': []}
+    with pytest.raises(DependencyError):
+        deps._verify_action_params(invalid_input)
+
+    # Dict mustn't be empty
+    invalid_input = {}
+    with pytest.raises(DependencyError):
+        deps._verify_action_params(invalid_input)
+
+    # Type of encoder param container should be list
+    invalid_input = {conf.param: {'ah', 'beh'}}
+    with pytest.raises(DependencyError):
+        deps._verify_action_params(invalid_input)
+
+    # Type of encoder param items should be str
+    invalid_input = {conf.param: [1]}
+    with pytest.raises(DependencyError):
+        deps._verify_action_params(invalid_input)
+
+    # Test valid
+    deps._verify_action_params({'valid': ['totally', 'valid']})
+
